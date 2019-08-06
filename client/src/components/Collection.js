e0fb991e7a3c4381f5725aa16530cafaa885a991
Chris-Humphrey <clhumphrey21@gmail.com>
2019-08-06 14:53:20 -0500

    pull

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -26,6 +26,8 @@ import {
   } from 'semantic-ui-react';
   import '../components/dashboard.css';
 
+  let sensorData = 'https://io.adafruit.com/api/feeds?x-aio-key=1de8b4e601e94f9a96f29c07626470c2';
+
 
 let jsonPlants = jsonData.plants
 class Collection extends React.Component {
@@ -35,7 +37,8 @@ class Collection extends React.Component {
         plants: jsonPlants,
         plant: [],
         collection: [],
-        remove: {}
+        remove: {},
+        sensor: 0
       }
       this.addToCollection = this.addToCollection.bind(this)
       this.displayCollection = this.displayCollection.bind(this)
@@ -91,7 +94,26 @@ async removeFromCollection(e, plants){
 
 componentDidMount(){
     this.displayCollection()
+    setInterval(() => {
+        this.sensorUpdate()
+    }, 5000);
   }
+
+sensorUpdate() {
+    fetch(sensorData)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        this.setState({
+            ...this.state,
+            sensor: data[0].last_value
+        })
+    })
+    .catch((error) => {
+        console.log('error', error)
+    })
+}
 
 // componentDidUpdate(prevProps){
 //   if(this.props.dbWishlist !== prevProps.dbWishlist){
@@ -101,7 +123,7 @@ componentDidMount(){
 
 render() {
   console.log('collection DB props')
-  console.log(this.props.dbCollection)
+  console.log(this.state.sensor)
     return (
         <>
         <div className='fluid-container'>
@@ -140,22 +162,31 @@ render() {
                     <Card.Description>
                     Moisture:
                     { (this.state.sensor <= 15) &&
+                    <>
                     <Label circular color={'red'}>
                         {this.state.sensor}
-                    </Label> &&
-                    <Image src="https://i.gifer.com/7Q0a.gif" style={{width: "100px"}} floated="right" rounded />
+                    </Label>
+                    <Card.Content>
+                         <Image src="https://i.gifer.com/7Q0a.gif" style={{width: "100px"}} floated="right" rounded />
+
+                    </Card.Content>
+                    </>
                     }
                     { (this.state.sensor <= 35 && this.state.sensor >= 16) &&
+                    <>
                     <Label circular color={'yellow'}>
                         {this.state.sensor}
-                    </Label> &&
+                    </Label>
                     <Image src="https://thumbs.gfycat.com/ChiefHeftyBasil-small.gif" style={{width: "100px"}} floated="right" rounded />
+                    </>
                     }
                     { (this.state.sensor <= 100 && this.state.sensor >= 36) &&
+                    <>
                     <Label circular color={'green'}>
                         {this.state.sensor}
-                    </Label> &&
+                    </Label>
                     <Image src="https://thumbs.gfycat.com/ChiefHeftyBasil-small.gif" style={{width: "100px"}} floated="right" rounded />
+                    </>
                     }
 
                     </Card.Description>
